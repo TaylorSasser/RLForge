9611946ad3bca5a26b1193b5fe6cd825e64c91a6
Taylor <Taylorsasser@gmail.com>
2018-05-23 11:24:51 -0400

    Hooks

--- RLForge/RLForge/Hooks/HookFactory.hpp
+++ RLForge/RLForge/Hooks/HookFactory.hpp
@@ -1,51 +1,40 @@
 #pragma once
 #include <type_traits>
+#include <memory>
+#include <functional>
+#include <optional>
 
 
-template<std::size_t N,class TypeOne,class... Args>
-struct Type
-{
-	using Type = typename Type<N - 1U,Args...>::Type
-};
-
-template<class TypeOne,class... Args>
-struct Type<0U,TypeOne,Args...>
-{
-	using Type = TypeOne;
-};
-
-template<std::size_t,class>
-struct Argument {};
-
-template<std::size_t N,class R,class... Args>
-struct Argument<N,R(Args...)>
-{
-	using Type = typename Type<N,Args...>::Type;
-};
-
-
-template<class>
-struct ReturnType{};
+#include "../RL/SDK.hpp"
 
 template<class Return,class... Args>
-struct ReturnType<Return(Args...)>
-{
-	using Type = Return;
-};
-
-
-
-
 class IFunctionHook {
 public:
-	
-	virtual IFunctionHook();
-	
+    typedef std::function<Return,Args...> FunctionSignature;
+
+	virtual std::optional<FunctionSignature> Setup(const FunctionSignature&) = 0;
+	virtual bool Restore(const FunctionSignature&) = 0;
 };
 
-
-class ProcessEventHook : public IFunctionHook
+class ProcessEventHook : public IFunctionHook<void(SDK::UObject**,SDK::UFunction*,void*)>
 {
+public:
+    explicit ProcessEventHook() = default;
+
+    /**
+     * Returns the OldProcessEvent Function if hook worked.
+     * @param Function Function to detour too.
+     * @return
+     */
+    virtual std::optional<FunctionSignature> Setup(const FunctionSignature& Function) {
+
+    }
+
+    virtual bool Restore(const FunctionSignature Function) {
+
+    }
+
+private:
 
 };
 
@@ -54,9 +43,22 @@ class ProcessEventHook : public IFunctionHook
 class HookFactory
 {
 public:
-	template<class T,class = std::enable_if_t<std::is_base_of<IFunctionHook,T>>>
-	static T New();
+	template<class T,class = std::enable_if_t<std::is_base_of_v<IFunctionHook,T>>>
+	static std::shared_ptr<T> New()
+	{
+	    return std::make_shared<T>();
+	}
+
+
 };
 
 
-auto ProcessEventHook = HookFactory::New<ProcessEventHook>();
+void ProcessEventProxy(SDK::UObject**,SDK::UFunction*,void* Params) {
+
+}
+
+int test() {
+    std::shared_ptr<ProcessEventHook> ProcessEventHook = HookFactory::New<ProcessEventHook>();
+
+    ProcessEventHook->Setup(std::bind<void,SDK::UObject**,SDK::UFunction*,void*>(&ProcessEventProxy));
+
