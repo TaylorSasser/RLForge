5e5864eb2e7e3837c5b38f4196928ddedd239e86
Taylor <Taylorsasser@gmail.com>
2018-06-06 10:25:42 -0400

    Finished Detour Hook

--- RLForge/RLForge/Hooks/Detour.hpp
+++ RLForge/RLForge/Hooks/Detour.hpp
@@ -26,25 +26,37 @@ public:
 		
 		if (!AllocateBuffer(reinterpret_cast<uint8_t*>(Pointer)))
 			return false;
-		
-		asmjit::CodeHolder ThunkHolder{};
-		asmjit::X86Assembler ThunkJump{&ThunkHolder};
-		
-		asmjit::CodeHolder HookHolder{};
-		asmjit::X86Assembler HookJump{&HookHolder};
+
+		asmjit::JitRuntime Runtime;
+
+
+		asmjit::CodeHolder ThunkHolder;
+		ThunkHolder.init(Runtime.getCodeInfo());
+		asmjit::X86Assembler ThunkJump(&ThunkHolder);
+
+		asmjit::CodeHolder HookHolder;
+		HookHolder.init(Runtime.getCodeInfo());
+		asmjit::X86Assembler HookJump(&HookHolder);
 		
 		ThunkJump.jmp(asmjit::x86::ptr_32(*BufferPointer));
 		this->OriginalSize = ThunkHolder.getCodeSize();
-		
 		CopyOldCode((uint8_t*)this->OriginalFunction);
-		
 		asmjit::X86Mem ThreadLocalStorage = asmjit::x86::dword_ptr_abs(0x14);
 		ThreadLocalStorage.setSegment(asmjit::x86::fs);
 		HookJump.mov(ThreadLocalStorage,asmjit::x86::eax);
-		
 		HookJump.jmp((asmjit::x86::ptr)(static_cast<uint32_t>(&HookHandler<Function,C>::HandlerWrapper)));
-		
 		HookHolder.relocate(this->BufferPointer);
+
 		HookHolder._codeInfo.setBaseAddress((uintptr_t)this->OriginalFunction);
+		ThunkHolder.relocate(this->TrampolineCode);
+
+		DWORD OldProtection;
+		if (!VirtualProtect(this->OriginalCode,this->OriginalSize,PAGE_EXECUTE_READWRITE,&OldProtection))
+		    return false;
+
+		memcpy(this->OriginalCode,this->TrampolineCode,this->OriginalSize);
+		VirtualProtect(this->OriginalCode,this->OriginalSize,OldProtection,&OldProtection);
+		this->isHooked = (this->OriginalSize != 0);
+
 	}
 };
